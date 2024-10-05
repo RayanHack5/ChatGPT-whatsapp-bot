7978402fd80b4c0adb1994a446a0d103b72bde26
rnKerm <149631529+RayanHack5@users.noreply.github.com>
2024-10-05 13:40:00 +0100

    Update whatsapp-config-example.js

--- config/whatsapp-config-example.js
+++ config/whatsapp-config-example.js
@@ -5,6 +5,6 @@
 const example_whatsapp_config = {
   allowedChats: [], // Array<String> - array of id's of every one to one chat the bot is allowed to write a response
   allowedGroups: [], // Array<String> - array of id's of every group chat the bot is allowed to write a response
-  phone: 'XXXXXXX', // String - phone number from the account added to Whatsapp web client
+  phone: '237659535227', // String - phone number from the account added to Whatsapp web client
   // It will be use to know when the bot got mentioned in a chat group - only then it will talk
 };
